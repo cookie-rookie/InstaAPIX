359fdf23b1eebcd72ed4a49f274b4e6ac3817b9a
cookie <cookiesummergames@gmail.com>
2024-08-29 23:42:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='InstaAPIX',
     version='0.1',
-    packages=find_packages(where='src'),  
-    package_dir={'': 'src'}, 
+     packages=(
+        find_packages() +
+        find_packages(where='./src')
+     ),  
     install_requires=[
         'requests',
     ],
